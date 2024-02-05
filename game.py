a7e73f0a5b64176909ad1b6236bf267d0f2731ea
sartoriusbg <119054239+sartoriusbg@users.noreply.github.com>
2024-02-05 15:13:18 +0200

    font changed

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ pygame.display.set_caption('Battleships')
 
 SCREEN_HEIGHT = 500
 SCREEN_WIDTH = 800
-main_font = pygame.font.SysFont("cambria", 35)
+button_font = pygame.font.SysFont("freesansbold.ttf", 35)
 
 
 class Button():
@@ -15,7 +15,7 @@ class Button():
 		self.y_pos = y_pos
 		self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
 		self.text_input = text_input
-		self.text = main_font.render(self.text_input, True, "white")
+		self.text = button_font.render(self.text_input, True, "white")
 		self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
 
 	def update(self):
@@ -28,9 +28,9 @@ class Button():
 
 	def changeColor(self, position):
 		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
-			self.text = main_font.render(self.text_input, True, "green")
+			self.text = button_font.render(self.text_input, True, "green")
 		else:
-			self.text = main_font.render(self.text_input, True, "white")
+			self.text = button_font.render(self.text_input, True, "white")
 
 button_surface = pygame.image.load("button.webp")
 button_surface = pygame.transform.scale(button_surface, (200, 50))
@@ -44,8 +44,8 @@ def debug_print(text):
 
 background = pygame.image.load('battleships.jpg')
 background = pygame.transform.scale(background, (800, 500))
-font = pygame.font.Font('freesansbold.ttf', 70)
-text = font.render('Battleships', True, "black")
+title_font = pygame.font.Font('freesansbold.ttf', 70)
+text = title_font.render('Battleships', True, "black")
 textRect = text.get_rect()
 textRect.center = (400, 100)
 running = True
